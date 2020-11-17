b3280638487637c97b1571ff50735382ad08d6fc
Jefferson Oña <67077796+jeffqev@users.noreply.github.com>
2020-11-17 17:35:43 -0500

    testing changes

--- server/server.go
+++ server/server.go
@@ -12,5 +12,9 @@ func Up() {
 		fmt.Fprintf(w, "Github actions CI/CD ")
 	})
 
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome")
+	})
+
 	log.Fatal(http.ListenAndServe(":1323", nil))
 }
